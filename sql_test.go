ab01e714f225cab585556b6a7562f115ff7bca38
harundarat <harundarat@gmail.com>
2025-07-24 06:50:51 +0700

    auto increment

--- sql_test.go
+++ sql_test.go
@@ -139,3 +139,22 @@ func TestExecSqlParameter(t *testing.T) {
 	}
 	fmt.Println("Success insert into user")
 }
+
+func TestAutoIncrement(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+	ctx := context.Background()
+	email := "harundarat@gmail.com"
+	comment := "test comment"
+	script := "INSERT INTO comments(email,comment) VALUES(?,?)"
+	result, err := db.ExecContext(ctx, script, email, comment)
+	if err != nil {
+		panic(err)
+	}
+	insertId, err := result.LastInsertId()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Success insert new comment with id", insertId)
+
+}
