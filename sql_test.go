b16ea59b3cb3f731cd9985f3167de00a470f99a3
harundarat <harundarat@gmail.com>
2025-07-23 07:40:07 +0700

    add SQL injection test case to validate user authentication

--- sql_test.go
+++ sql_test.go
@@ -77,3 +77,27 @@ func TestQuerySqlComplex(t *testing.T) {
 		fmt.Println("=======================================================================================")
 	}
 }
+
+func TestSqlInjection(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+	ctx := context.Background()
+	username := "admin'; #"
+	password := "salah"
+	script := "SELECT username FROM user WHERE username = '" + username + "' AND password = '" + password + "' LIMIT 1"
+	rows, err := db.QueryContext(ctx, script)
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+	if rows.Next() {
+		var username string
+		err := rows.Scan(&username)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Login success", username)
+	} else {
+		fmt.Println("Login failed")
+	}
+}
