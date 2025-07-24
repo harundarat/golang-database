172b060a8f10a7b36997befe071d14018bc03f79
harundarat <harundarat@gmail.com>
2025-07-24 07:21:35 +0700

    db transaction

--- sql_test.go
+++ sql_test.go
@@ -183,3 +183,33 @@ func TestPrepareStatement(t *testing.T) {
 		fmt.Println("Comment Id ", id)
 	}
 }
+
+func TestTransaction(t *testing.T) {
+	db := GetConnection()
+	defer db.Close()
+	ctx := context.Background()
+	tx, err := db.Begin()
+	if err != nil {
+		panic(err)
+	}
+	// do transaction here
+	script := "INSERT INTO comments(email, comment) VALUES(?, ?)"
+	for i := 0; i < 10; i++ {
+		email := "harun" + strconv.Itoa(i) + "@gmail.com"
+		comment := "Komentar ke " + strconv.Itoa(i)
+		result, err := tx.ExecContext(ctx, script, email, comment)
+		if err != nil {
+			panic(err)
+		}
+		id, err := result.LastInsertId()
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("Comment Id ", id)
+	}
+	// err = tx.Commit()
+	err = tx.Rollback()
+	if err != nil {
+		panic(err)
+	}
+}
